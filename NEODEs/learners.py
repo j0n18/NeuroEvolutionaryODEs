2dbab0d75d70c6b51a773fe69602bb5e739d27a6
j0n18 <jonathan.d.mccart@gmail.com>
2022-04-19 16:32:45 -0400

    add trajectory learner (work in progress)

--- NEODEs/learners.py
+++ NEODEs/learners.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.utils.data as data
 import pytorch_lightning as pl
 
-class BaseLearner(pl.LightningModule):
+class ClassificationLearner(pl.LightningModule):
     def __init__(self, 
                 X_data: torch.Tensor, 
                 y_data: torch.Tensor,
@@ -19,6 +19,7 @@ class BaseLearner(pl.LightningModule):
         return self.model(x)
     
     def training_step(self, batch, batch_idx):
+        #import pdb; pdb.set_trace();
         x, y = batch      
         t_eval, y_hat = self.model(x, self.t_span)
         y_hat = y_hat[-1] # select last point of solution trajectory
@@ -40,3 +41,70 @@ class BaseLearner(pl.LightningModule):
         trainloader = data.DataLoader(train, batch_size=len(X), shuffle=True)
 
         return trainloader
+
+
+class TrajectoryLearner(pl.LightningModule):
+    def __init__(self, 
+                datamodule: pl.LightningDataModule,
+                model:pl.LightningModule):
+
+        super().__init__()
+
+        self.model = model
+        self.datamodule = datamodule
+    
+    def forward(self, x):
+        return self.model(x)
+    
+    def training_step(self, batch, batch_idx):
+        x, t = batch   
+
+        #NeurlODE model handles single sample operations,
+        #so batch the data in here:
+
+        batch_size, _, _ = x.size()
+        x_hat = torch.zeros_like(x)
+
+        for bat in range(batch_size):
+            x_hat[bat] = self.model(x[bat].squeeze(), t[bat].squeeze())
+
+        #loss: difference between true trajectory
+        #and the evolved Neural ODE trajectory
+        mse = nn.MSELoss()
+        loss = mse(x_hat, x)
+
+        return {'loss': loss}   
+
+    def validation_step(self, batch, batch_idx):
+
+        import pdb; pdb.set_trace();
+
+        x, t = batch 
+        
+        #NeurlODE model handles single sample operations,
+        #so batch the data in here:
+
+        batch_size, _, _ = x.size()
+        x_hat = torch.zeros_like(x)
+
+        for bat in range(batch_size):
+            x_hat[bat] = self.model(x[bat].squeeze(), t[bat].squeeze())
+
+        #loss: difference between true trajectory
+        #and the evolved Neural ODE trajectory
+        mse = nn.MSELoss()
+        loss = mse(x_hat, x)
+
+        return {'loss': loss} 
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=0.01)
+
+    def train_dataloader(self):
+        return self.datamodule.train_dataloader()
+
+    def val_dataloader(self):
+        return self.datamodule.val_dataloader()
+
+    def test_dataloader(self):
+        return self.datamodule.test_dataloader()
