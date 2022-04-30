b80a5241accecb3e95406737a5c59e4b2b89f7db
j0n18 <jonathan.d.mccart@gmail.com>
2022-04-30 17:10:09 -0400

    utils: revert to r2 score

--- NEODEs/utils.py
+++ NEODEs/utils.py
@@ -1,11 +1,7 @@
-import pytorch_lightning as pl
-from torchdyn.core import NeuralODE
-from neat.phenotype.feed_forward import FeedForwardNet
 import torch
-from data import NeuralODEDataModule
-from sklearn.metrics import r2_score
-import numpy as np
 import torch.nn.functional as F
+import numpy as np
+from sklearn.metrics import r2_score
 
 def instantiate_dataloader(datamodule, phase_flag):
 
@@ -17,6 +13,8 @@ def instantiate_dataloader(datamodule, phase_flag):
         return datamodule.train_dataloader()
     elif phase_flag == "val":
         return datamodule.val_dataloader()
+    elif phase_flag == "test":
+        return datamodule.test_dataloader()
 
 
 
@@ -34,6 +32,12 @@ def NEODE_fwd(model, datamodule, phase_flag = "val"):
         n_samples, *_ = datamodule.valid_ds.tensors[0].size()
         num_batches = n_samples // datamodule.hparams.batch_size
         x_node = torch.zeros_like(datamodule.train_ds.tensors[0])
+        t_node = torch.zeros((n_samples, 2))
+
+    elif phase_flag == "test":
+        n_samples, *_ = datamodule.test_ds.tensors[0].size()
+        num_batches = n_samples // datamodule.hparams.batch_size
+        x_node = torch.zeros_like(datamodule.test_ds.tensors[0])
         t_node = torch.zeros((n_samples, 2))
 
     
@@ -63,17 +67,13 @@ def NEODE_fwd(model, datamodule, phase_flag = "val"):
 
 
 def get_similarity_score(valid_data, node_data):
-    '''
-    Similarity is measured using the average cosine similarity
-    evaluated at each point along the trajectory.
-    '''
-    
-    valid_data = valid_data.detach()
-    node_data = node_data.detach()
+    #import pdb; pdb.set_trace();
+    valid_data = valid_data.detach().cpu().numpy()
+    node_data = node_data.detach().cpu().numpy()
+    valid_data = np.concatenate([*valid_data])
+    node_data = np.concatenate([*node_data])
+    r2 = r2_score(valid_data, node_data)
 
-    valid_data = valid_data.reshape(-1, valid_data.shape[-1])
-    node_data = node_data.reshape(-1, node_data.shape[-1])
-
-    return F.cosine_similarity(valid_data,node_data).mean()
+    return r2
 
 
