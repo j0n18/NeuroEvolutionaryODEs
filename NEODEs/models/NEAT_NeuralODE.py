123bb438f9356cc4db472ba30d0d39fb3efbd73c
j0n18 <jonathan.d.mccart@gmail.com>
2022-04-30 12:18:04 -0400

    fix NEAT NODE

--- NEODEs/models/NEAT_NeuralODE.py
+++ NEODEs/models/NEAT_NeuralODE.py
@@ -1,29 +1,49 @@
 import pytorch_lightning as pl
 from torchdyn.core import NeuralODE
-from NEODEs.pytorch_neat.neat.phenotype.feed_forward import FeedForwardNet
+from neat.phenotype.feed_forward import FeedForwardNet
 import torch
+import torch.nn as nn
+
+class NEATVectorField(nn.Module):
+
+    def __init__(self, ffn):
+        super().__init__()
+        self.ffn = ffn
+
+    def forward(self, t, x):
+
+        n_timesteps, dim = x.size()
+
+        output = torch.zeros_like(x)
+
+        for t in range(n_timesteps): 
+            output[t] = self.ffn(x[t].unsqueeze(0))
+
+        return output
+
+    def __call__(self, t, x):
+        
+        return self.forward(t,x)
+
+
 
 class NEAT_NeuralODE(pl.LightningModule):
-
 
     def __init__(self, genome, config):
 
         super().__init__()
 
-        #instantiate the FFN we want to use in the Neural ODE:
-        #Note: NeuralODE() returns t_vals, sol by default
-        #https://github.com/DiffEqML/torchdyn/blob/master/torchdyn/core/neuralde.py#L33
-
         self.ffn = FeedForwardNet(genome, config)
-        self.model = NeuralODE(self.ffn, 
+        self.vf = NEATVectorField(self.ffn)
+
+        self.model = NeuralODE(self.vf, 
                                 sensitivity='adjoint', 
                                 solver='tsit5', 
-                                #interpolator=None, 
+                                interpolator=None, 
                                 atol=1e-3, rtol=1e-3)
 
         self.save_hyperparameters()
 
     def forward(self, x, t_span):
-        #import pdb; pdb.set_trace();
 
-        return self.model(x,t_span) #.trajectory(x,t_span) #[-1]
+        return self.model(x,t_span) 
